f3b0414ac74b532511e105f365ca02868a90a02b
Alex Perrin <>
2023-12-26 15:31:54 +0000

    update configs, add secrets helper

--- handler.py
+++ handler.py
@@ -2,23 +2,4 @@ import json
 
 
 def lambda_handler(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    print("hello world!")
