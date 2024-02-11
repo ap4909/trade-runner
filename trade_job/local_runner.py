a340b6052bea5512fa1426d455c1325cd98ae2c0
Alex Perrin <>
2024-02-11 12:35:01 +0000

    add parameters to local runner

--- trade_job/local_runner.py
+++ trade_job/local_runner.py
@@ -1,6 +1,9 @@
 from trade_run import start_trade_run
 
 if __name__ == "__main__":
-    event = {"symbol": "AAPL",
-             "offset": 2586}
+    event = {
+        "minimumPoints": 3,
+        "symbol": "AAPL",
+        "offsetTime": 2160,
+        "windowLength": 5}
     start_trade_run(event, None)
